8bfde37770eb55c7572fbb0fcedb1855d357d38a
bai <932236581@qq.com>
2023-11-29 23:35:45 +0800

    创建mudule

---
+++ app/src/main/java/com/hx/admobileapicloud/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hx.admobileapicloud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.admobiletop.adsuyi.ADSuyiSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
